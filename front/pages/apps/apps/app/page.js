d392a5fe8a89235b7514b3a3fdf6b6377feb5aa3
JUN-HYEOK SEO <starlights2283@gmail.com>
2023-08-09 13:40:23 +0000

    next.js -apps page dev gen

---
+++ front/pages/apps/apps/app/page.js
@@ -0,0 +1,14 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    // <div className='hello-ax'>Hello_USERS</div>
+    <div className='hb'>
+      <div className='ds1'>
+        <div className='auth'>
+          회원가입
+        </div>
+      </div>
+    </div>
+  )
+}
